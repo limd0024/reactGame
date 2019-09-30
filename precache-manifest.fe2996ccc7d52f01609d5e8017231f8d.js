6e7b906bfde826943fbcfa72de1ba8b5080d36f3
Limd0024 <limdixiang93@gmail.com>
2019-09-30 11:39:40 +0800

    Updates

---
+++ precache-manifest.fe2996ccc7d52f01609d5e8017231f8d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88c85046aa766232c794c2da0aab06b5",
+    "url": "/reactGame/index.html"
+  },
+  {
+    "revision": "60f2928734bb40a02f5f",
+    "url": "/reactGame/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "76a65423e3ce6c23bf0b",
+    "url": "/reactGame/static/css/main.8f2004ee.chunk.css"
+  },
+  {
+    "revision": "60f2928734bb40a02f5f",
+    "url": "/reactGame/static/js/2.6b90180f.chunk.js"
+  },
+  {
+    "revision": "76a65423e3ce6c23bf0b",
+    "url": "/reactGame/static/js/main.79115535.chunk.js"
+  },
+  {
+    "revision": "0df363fb1d4938da3f6b",
+    "url": "/reactGame/static/js/runtime~main.e8b1c48b.js"
+  },
+  {
+    "revision": "8817e8633b7bd64455370f707943ceb3",
+    "url": "/reactGame/static/media/BattleFieldImg.8817e863.jpg"
+  }
+]);
